575af6f4f393d0b7ed237f5128571e6e0ad0522e
Christian Stein <sormuras@gmail.com>
2019-06-05 08:40:06 +0200

    Add newFixedThreadPool(4) sample

--- src/test/java/DoneTests.java
+++ src/test/java/DoneTests.java
@@ -3,8 +3,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.TimeUnit;
 
 @Execution(ExecutionMode.CONCURRENT)
 class DoneTests {
@@ -41,27 +46,50 @@ class DoneTests {
   private void work(int id)  {
     System.out.println("BEGIN " + id);
     System.out.println(Thread.currentThread().getName());
-    ForkJoinPool customThreadPool = new ForkJoinPool(4);
-    RecursiveAction a = new RecursiveAction() {
-      @Override
-      protected void compute() {
-        while (true) {
-          try {
-            Thread.sleep(1000);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-          System.out.println(Thread.currentThread().getName());
-        }
-      }
-    };
-    customThreadPool.invoke(a);
 
+    // SHOWS THE ISSUE
+//    ForkJoinPool customThreadPool = new ForkJoinPool(4);
+//    RecursiveAction a = new RecursiveAction() {
+//      @Override
+//      protected void compute() {
+//        while (true) {
+//          try {
+//            Thread.sleep(1000);
+//          } catch (InterruptedException e) {
+//            e.printStackTrace();
+//          }
+//          System.out.println(id + " -> " + Thread.currentThread().getName());
+//        }
+//      }
+//    };
+//    customThreadPool.invoke(a);
+
+    // WORKS AS EXPECTED
 //    try {
 //      Thread.sleep(Long.MAX_VALUE);
 //    } catch (InterruptedException e) {
 //      e.printStackTrace();
 //    }
+
+    // WORKS AS EXPECTED
+    ExecutorService service = Executors.newFixedThreadPool(4);
+    Callable<Void> a = () -> {
+      while (true) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println(id + " -> " + Thread.currentThread().getName());
+      }
+    };
+    try {
+      service.invokeAll(List.of(a));
+      service.shutdown();
+      service.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
     System.out.println("DONE " + id);
   }
